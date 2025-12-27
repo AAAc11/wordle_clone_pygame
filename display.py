22e750d8faa974771fbf910c1aca751c01c1770c
Anna <annacygan344@gmail.com>
2025-12-27 17:10:52 +0100

    Dodanie klasy oraz liter, zmiana sposobu wyświetlania klawiatury

--- display.py
+++ display.py
@@ -15,25 +15,29 @@ text = font.render('WORDLE', True, WHITE)
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 window.fill(WINDOW_COLOR)
 pygame.display.set_caption("WORDLE")
-window.blit(text, [165, 30])
-pygame.display.update()
 
-x = 130
-y = 100
-for i in range(0,5):
-    for j in range(0, 5):
-        RECT_PARAMETERS = pygame.Rect(x, y, 50, 50)
-        pygame.draw.rect(window, WHITE, RECT_PARAMETERS, 2)
-        x += 60
-        j += 1
+running = True
+while running:
+    window.fill(WINDOW_COLOR)
+    window.blit(text, [165, 30])
+
     x = 130
-    y += 60
-    i += 1
+    y = 100
+    for i in range(5):
+        for j in range(5):
+            RECT_PARAMETERS = pygame.Rect(x, y, 50, 50)
+            pygame.draw.rect(window, WHITE, RECT_PARAMETERS, 2)
+            x += 60
+        x = 130
+        y += 60
 
-    keyboard_display(window, WHITE)
 
-pygame.display.flip()
-while True:
+    keyboard_display(window)
+    pygame.display.flip()
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit(0)
+            running = False
+
+pygame.quit()
--- keyboard.py
+++ keyboard.py
@@ -1,15 +1,49 @@
 import pygame
 
+pygame.font.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+rows_of_letters = ['QWERTYUIOP',
+                   'ASDFGHJKL',
+                   'ZXCVBNM']
+buttons = {}
+
+class Button:
+    def __init__(self, letter, background_color, width, x, y, font_size, window):
+        self.letter =letter
+        self.background_color = background_color
+        self.width = width
+        self.x = x
+        self.y = y
+        self.font_size = font_size
+        self.window = window
+
+    def creating_area(self):
+        font = pygame.font.Font(None, self.font_size)
+        surface = pygame.draw.rect(self.window, self.background_color, (self.x, self.y, self.width, 55), 0, border_radius=3)
+        letter_surface = font.render(self.letter, True, BLACK)
+        letter_rect = letter_surface.get_rect(center = surface.center)
+        self.window.blit(letter_surface, letter_rect)
+
+    def change_color(self, color):
+        self.background_color = color
+
 #rows of letters
-def keyboard_display(window, frame_color):
-    for i in range(30, 521, 50):
-        pygame.draw.rect(window, frame_color, (i, 450, 40, 55), 0, border_radius=3)
+def keyboard_display(window):
+    start_x_positions = [30, 55, 105]
 
-    for i in range(55, 500, 50):
-        pygame.draw.rect(window, frame_color, (i, 520, 40, 55), 0, border_radius=3)
+    for i, (row_letters, y) in enumerate(zip(rows_of_letters, range(450, 591, 70))):
+        current_x = start_x_positions[i]
 
-    for i in range(105, 450, 50):
-        pygame.draw.rect(window, frame_color, (i, 590, 40, 55), 0, border_radius=3)
+        for letter in row_letters:
+            new_letter = Button(letter, WHITE, 40, current_x, y, 35, window)
+            buttons[letter] = new_letter
+            new_letter.creating_area()
+            current_x += 50
 
-    pygame.draw.rect(window, frame_color, (30, 590, 65, 55), 0, border_radius=3)
-    pygame.draw.rect(window, frame_color, (455, 590, 60, 55), 0, border_radius=3)
+    enter = Button('ENTER', WHITE, 65, 30, 590, 20, window)
+    enter.creating_area()
+    backspace = Button('RETURN', WHITE, 65, 455, 590, 20, window)
+    backspace.creating_area()
